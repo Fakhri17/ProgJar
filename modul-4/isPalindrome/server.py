be713c139894546245a6b6e6d686525a86a3d055
Fakhri17 <fakhricoffe@gmail.com>
2024-03-27 23:54:51 +0700

    fix modul coyy

---
+++ modul-4/isPalindrome/server.py
@@ -0,0 +1,24 @@
+import socket
+
+# Inisialisasi socket UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind socket ke alamat dan port tertentu
+server_address = ('localhost', 5000)
+sock.bind(server_address)
+
+while True:
+  print('Menunggu pesan dari client...')
+
+  # Menerima pesan dari client
+  data, address = sock.recvfrom(4096)
+
+  # palindrome
+  message = data.decode()
+  if message == message[::-1]:
+    message = 'Palindrome'
+  else:
+    message = 'Bukan palindrome'
+
+  # Mengirim balasan ke client apakah palindrome atau bukan
+  sock.sendto(message.encode(), address)
---
+++ modul-4/sum-integer/server.py
@@ -0,0 +1,23 @@
+import socket
+
+# Inisialisasi socket UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind socket ke alamat dan port tertentu
+server_address = ('localhost', 5000)
+sock.bind(server_address)
+
+while True:
+  print('Menunggu pesan dari client...')
+
+  # Menerima pesan dari client
+  data, address = sock.recvfrom(4096)
+
+  # Menghitung jumlah bilangan bulat dari pesan yang diterima
+  numbers = list(map(int, data.decode().split(',')))
+  sum_integer = sum(numbers)
+
+  # Mengirim balasan ke client berupa jumlah bilangan bulat
+  message = str(sum_integer)
+
+  sock.sendto(message.encode(), address)
